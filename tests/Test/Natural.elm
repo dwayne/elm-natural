fe71ce01140d4647f5ec489882ff630384f5acda
Dwayne Crooks <me@dwaynecrooks.com>
2023-01-03 04:58:54 -0400

    Test the constants

--- tests/Test/Natural.elm
+++ tests/Test/Natural.elm
@@ -10,6 +10,7 @@ suite : Test
 suite =
     describe "Natural"
         [ intConversionSuite
+        , constantsSuite
         ]
 
 
@@ -34,6 +35,56 @@ intConversionSuite =
                 Natural.fromInt maxSafeInteger
                     |> Maybe.map Natural.toInt
                     |> Expect.equal (Just maxSafeInteger)
+        ]
+
+
+constantsSuite : Test
+constantsSuite =
+    describe "constants"
+        [ test "0" <|
+            \_ ->
+                Just Natural.zero
+                    |> Expect.equal (Natural.fromInt 0)
+        , test "1" <|
+            \_ ->
+                Just Natural.one
+                    |> Expect.equal (Natural.fromInt 1)
+        , test "2" <|
+            \_ ->
+                Just Natural.two
+                    |> Expect.equal (Natural.fromInt 2)
+        , test "3" <|
+            \_ ->
+                Just Natural.three
+                    |> Expect.equal (Natural.fromInt 3)
+        , test "4" <|
+            \_ ->
+                Just Natural.four
+                    |> Expect.equal (Natural.fromInt 4)
+        , test "5" <|
+            \_ ->
+                Just Natural.five
+                    |> Expect.equal (Natural.fromInt 5)
+        , test "6" <|
+            \_ ->
+                Just Natural.six
+                    |> Expect.equal (Natural.fromInt 6)
+        , test "7" <|
+            \_ ->
+                Just Natural.seven
+                    |> Expect.equal (Natural.fromInt 7)
+        , test "8" <|
+            \_ ->
+                Just Natural.eight
+                    |> Expect.equal (Natural.fromInt 8)
+        , test "9" <|
+            \_ ->
+                Just Natural.nine
+                    |> Expect.equal (Natural.fromInt 9)
+        , test "10" <|
+            \_ ->
+                Just Natural.ten
+                    |> Expect.equal (Natural.fromInt 10)
         ]
 
 
